261a74afad17a8e2c784d52321a211f068c34197
Matrixas8 <mm3082426@gmail.com>
2025-06-06 20:19:03 -0300

    cambio el puerto para render

--- server.js
+++ server.js
@@ -9,4 +9,8 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
